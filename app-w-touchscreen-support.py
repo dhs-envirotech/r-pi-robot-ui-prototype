376c795cff5df09aa94ebc5c47439cde15541e5a
Mac <mac@Macs-MacBook-Pro.local>
2024-08-29 11:25:14 -0700

    Added some basic flask gpio, joystick examples

---
+++ app-w-touchscreen-support.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+import time
+
+app = Flask(__name__)
+
+# Set up GPIO for motor control
+motor_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(motor_pin, GPIO.OUT)
+pwm = GPIO.PWM(motor_pin, 1000)
+pwm.start(0)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/control', methods=['POST'])
+def control():
+    data = request.get_json()
+    angle = data['angle']
+    force = data['force']
+
+    # Convert joystick data to PWM duty cycle
+    duty_cycle = (force / 100) * 100  # Normalize force to PWM duty cycle
+
+    # Apply PWM to control motor speed
+    pwm.ChangeDutyCycle(duty_cycle)
+
+    return 'OK'
+
+@app.route('/stop', methods=['POST'])
+def stop():
+    # Stop the motor when the joystick is released
+    pwm.ChangeDutyCycle(0)
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True)
